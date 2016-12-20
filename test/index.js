a91a081b5aa0b3f0013aab352d407cebec39b4e3
Billy Janitsch <billy@kensho.com>
2016-12-20 13:43:02 -0500

    Improve robustness of test suite
    In addition to specifying the total number of expected lint errors
    in each fixture, specify line numbers and rules for each. This
    ensures that the build will still break if the config is modified
    in a way that happens to both introduce and remove a lint error
    from any of the fixtures.
    
    This also fixes a test error introduced in commit
    a9d57274e89d81c547f74f853d3565267328b61c

--- test/index.js
+++ test/index.js
@@ -1,15 +1,37 @@
 import test from 'ava'
 import {lint} from 'stylelint'
 
-const invalid = async (t, input, errors = 1) => {
-  // TODO: improve error messages
+const macro = async (t, input, expected) => {
   const result = await lint({files: `${__dirname}/fixtures/${input}.css`})
-  t.is(result.results[0].warnings.length, errors)
+  t.is(result.results.length, 1, 'linted exactly one file')
+  const actual = result.results[0]
+  t.is(actual.invalidOptionWarnings.length, 0, 'config did not contain invalid options')
+  t.is(expected.length, actual.warnings.length, 'found the correct number of errors')
+  expected.forEach(([line, rule]) => {
+    const error = actual.warnings.find(warning => warning.line === line && warning.rule === rule)
+    t.truthy(error, `found error ${rule} on line ${line}`)
+  })
 }
 
-invalid.title = provided => `lints issues with ${provided}`
+macro.title = provided => `lints issues with ${provided}`
 
-test('at rules', invalid, 'at')
-test('selectors', invalid, 'selectors', 2)
-test('line breaks', invalid, 'linebreaks', 5)
-test('properties', invalid, 'properties')
+test('at rules', macro, 'at', [
+  [5, 'at-rule-no-unknown'],
+])
+
+test('selectors', macro, 'selectors', [
+  [1, 'selector-no-id'],
+  [5, 'selector-class-pattern'],
+])
+
+test('line breaks', macro, 'linebreaks', [
+  [4, 'rule-non-nested-empty-line-before'],
+  [9, 'max-empty-lines'],
+  [16, 'declaration-empty-line-before'],
+  [23, 'rule-nested-empty-line-before'],
+  [28, 'max-empty-lines'],
+])
+
+test('properties', macro, 'properties', [
+  [4, 'property-no-unknown'],
+])

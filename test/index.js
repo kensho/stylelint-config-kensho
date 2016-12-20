kensho/stylelint-config-kensho

commit a9d57274e89d81c547f74f853d3565267328b61c

Enforce camelCase class selectors
Enable 'selector-class-pattern' to enforce camelCase in class
selectors to conform to our convention of importing styles in JS
using css-modules.

test/fixtures/selectors.css
@@ -1,3 +1,11 @@
-#example { /* use of id selector */
+#example { /* id selector */
+  color: white;
+}
+
+.example-class { /* non-camelCased class */
+  color: white;
+}
+
+.exampleClass {
   color: white;
 }

test/index.js
@@ -10,6 +10,6 @@ const invalid = async (t, input, errors = 1) => {
 invalid.title = provided => `lints issues with ${provided}`
 
 test('at rules', invalid, 'at')
-test('selectors', invalid, 'selectors')
+test('selectors', invalid, 'selectors', 2)
 test('line breaks', invalid, 'linebreaks', 5)
 test('properties', invalid, 'properties')

index.js
@@ -36,5 +36,8 @@ module.exports = {
       except: ['blockless-group', 'all-nested'],
       ignore: ['after-comment'],
     }],
+
+    // enforce camelCase class selectors
+    'selector-class-pattern': '^[a-z]+([A-Z][a-z]*)*$',
   },
 }

commit a91a081b5aa0b3f0013aab352d407cebec39b4e3

Improve robustness of test suite
In addition to specifying the total number of expected lint errors
in each fixture, specify line numbers and rules for each. This
ensures that the build will still break if the config is modified
in a way that happens to both introduce and remove a lint error
from any of the fixtures.

This also fixes a test error introduced in commit
a9d57274e89d81c547f74f853d3565267328b61c

test/fixtures/at.css
@@ -2,5 +2,5 @@
 
 .myClass {
   @mixin myMixin 1, 2;
-  @unknown something; /* unknown at rule */
+  @unknown atrule;
 }

test/fixtures/linebreaks.css
@@ -1,16 +1,16 @@
 .fine {
   color: white;
 }
-.missing-newline-before-block {
+.missingNewlineBeforeBlock {
   color: black;
 }
 
 
-.extra-newline-before-block {
+.extraNewlineBeforeBlock {
   color: black;
 }
 
-.extra-newline-between-decls {
+.extraNewlineBetweenProperties {
   color: white;
 
   background-color: black;
@@ -20,12 +20,12 @@
   .fine {
     color: white;
   }
-  .missing-newline-before-nested-block {
+  .missingNewLineBeforeNestedBlock {
     color: white;
   }
 
 
-  .extra-newline-before-nested-block {
+  .extraNewlineBeforeNestedBlock {
     color: white;
   }
 }

test/fixtures/properties.css
@@ -1,5 +1,5 @@
 .example {
   color: blue;
   composes: a;
-  unknown: blue; /* unknown property */
+  unknown-property: blue;
 }

test/fixtures/selectors.css
@@ -1,11 +1,11 @@
-#example { /* id selector */
+#idSelector {
   color: white;
 }
 
-.example-class { /* non-camelCased class */
+.non-camel-cased-class {
   color: white;
 }
 
-.exampleClass {
+.camelCasedClass {
   color: white;
 }

test/index.js
@@ -1,15 +1,37 @@
 import test from 'ava'
 import {lint} from 'stylelint'
 
-const invalid = async (t, input, errors = 1) => {
-  // TODO: improve error messages
+const macro = async (t, input, expected) => {
   const result = await lint({files: `${__dirname}/fixtures/${input}.css`})
-  t.is(result.results[0].warnings.length, errors)
+  t.is(result.results.length, 1, 'linted exactly one file')
+  const actual = result.results[0]
+  t.is(actual.invalidOptionWarnings.length, 0, 'config did not contain invalid options')
+  t.is(expected.length, actual.warnings.length, 'found the correct number of errors')
+  expected.forEach(([line, rule]) => {
+    const error = actual.warnings.find(warning => warning.line === line && warning.rule === rule)
+    t.truthy(error, `found error ${rule} on line ${line}`)
+  })
 }
 
-invalid.title = provided => `lints issues with ${provided}`
+macro.title = provided => `lints issues with ${provided}`
 
-test('at rules', invalid, 'at')
-test('selectors', invalid, 'selectors', 2)
-test('line breaks', invalid, 'linebreaks', 5)
-test('properties', invalid, 'properties')
+test('at rules', macro, 'at', [
+  [5, 'at-rule-no-unknown'],
+])
+
+test('selectors', macro, 'selectors', [
+  [1, 'selector-no-id'],
+  [5, 'selector-class-pattern'],
+])
+
+test('line breaks', macro, 'linebreaks', [
+  [4, 'rule-non-nested-empty-line-before'],
+  [9, 'max-empty-lines'],
+  [16, 'declaration-empty-line-before'],
+  [23, 'rule-nested-empty-line-before'],
+  [28, 'max-empty-lines'],
+])
+
+test('properties', macro, 'properties', [
+  [4, 'property-no-unknown'],
+])